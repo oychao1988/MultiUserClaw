5df5d227ccb24ec6d1e2b4ce1dd83e06a9874622
Johnson <linghuchongxajh@qq.com>
2026-03-27 16:03:05 +0800

    "字体问题导致微信二维码显示不完整bug"

--- simple_front/src/pages/WeChat.tsx
+++ simple_front/src/pages/WeChat.tsx
@@ -307,7 +307,7 @@ function WeixinBindPanel({ onDone }: { onDone: () => void }) {
 
       <div
         ref={outputRef}
-        className="flex-1 overflow-auto whitespace-pre-wrap bg-black px-6 py-4 font-mono text-xs leading-relaxed text-green-200"
+        className="flex-1 overflow-auto whitespace-pre-wrap bg-black px-5 py-4 font-mono text-xs leading-none text-green-200"
       >
         {output || '正在连接微信绑定终端...'}
       </div>
